49d6840ea2a37a6ea4093601eaaaa41c876b8504
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-09-16 15:24:27 -0300

    Support to native gradle

---
+++ 1.8/test/test-app-gradlew/src/main/java/com/example/TestAppApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppApplication.class, args);
+	}
+}
