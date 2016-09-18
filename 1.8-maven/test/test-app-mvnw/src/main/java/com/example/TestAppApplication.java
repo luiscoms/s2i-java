38bc0d2318ee1cbe69aaf1801fae532b9b10dbe5
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-09-18 14:13:11 -0300

    Fix maven tests

--- 1.8-maven/test/test-app-mvnw/src/main/java/com/example/TestAppApplication.java
+++ 1.8-maven/test/test-app-mvnw/src/main/java/com/example/TestAppApplication.java
@@ -6,19 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RestController
-public class HelloController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-
-}
-
 @SpringBootApplication
 public class TestAppApplication {
 
