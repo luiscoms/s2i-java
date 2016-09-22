99eb9f41ed60fa25e3af1760b124a87032aa5d6b
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-09-22 03:06:30 -0300

    Add HelloController

--- 1.8-maven/test/test-app/src/main/java/com/example/App.java
+++ 1.8-maven/test/test-app/src/main/java/com/example/App.java
@@ -1,21 +1,24 @@
 package com.example;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import java.util.Arrays;
 
-@Controller
-@EnableAutoConfiguration
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class App {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(TestAppApplication.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(App.class, args);
-    }
 }
